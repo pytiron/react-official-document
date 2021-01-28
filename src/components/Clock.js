57eb6df911f87b72a47327dee81305241dfcdc61
lcs <cslee@itmoulds.com>
2021-01-28 16:19:20 +0900

    컴포넌트와 2개의 생명주기에 대한 흐름 설명

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -35,3 +35,10 @@ export default class Clock extends Component {
     )
   }
 }
+
+
+// 1. 생성자 호출하여 state 초기화
+// 2. render() 호출하여 DOM 노드들 생성/업데이트
+// 3. componentDidMount() 생명주기 메서드 호출
+// 4. setState 에 의한 변경 추적으로 render() 재호출/업데이트
+// 5. 컴포넌트 DOM으로부터 한 번이라도 삭제된 적이 있다면 componentWillUnmount() 생명주기 메서드 호출
