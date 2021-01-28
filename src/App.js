50f12367774e10badcbdc078fd96c5b19cf29e28
lcs <cslee@itmoulds.com>
2021-01-28 10:39:30 +0900

    element 공부

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 function App() {
     return (
         <div className="App">
+
             <h1>Hello, World</h1>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// 컴포넌트가 아닌 엘리먼트 객체로 DOM 노드를 그릴 수 있다.
+// element 객체는 한번 생성되면 불변성으로 인해 변할 수 없다. 따라서 변화 시키기 위해서는 변화 후
+// ReactDOM.render 다시 호출하여 변경사항을 적용시켜줘야 한다.
+// const elements = <h1>Hello, world</h1>
+// ReactDOM.render(elements, document.getElementById('root'))
 ReactDOM.render(
   <React.StrictMode>
     <App />
