7ebc5d4d8adbd74fa455ebec791127af88d004d7
lcs <cslee@itmoulds.com>
2021-01-28 14:08:37 +0900

    컴포넌트 순수함수 룰

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -5,6 +5,7 @@ function formatDate(date) {
   return date.toLocaleDateString();
 }
 
+// 매개변수에 대해서 순수함수처럼 동작해야한다는 법칙을 갖고 있다.
 export default function Comment(props) {
   return (
     <div className="Comment">
